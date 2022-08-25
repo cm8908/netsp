cec1f856f1c294cd3944817abc12a9a90899880d
cm8908 <jungminseob2@gmail.com>
2022-08-25 12:21:21 +0900

    minor change

--- train.py
+++ train.py
@@ -4,14 +4,20 @@ from model import NETSP
 from argparse import ArgumentParser
 from litmodule import LitNETSP
 from litdata import LitTSPDataModule
+# from pytorch_lightning.loggers import TensorBoardLogger
 # Specifying hyperparameters
 # https://pytorch-lightning.readthedocs.io/en/latest/common/hyperparameters.html
 
 # Modifying progress bar
 # https://forums.pytorchlightning.ai/t/how-to-modify-the-default-progress-bar/224/3
 
+# Logger version & directory names
+# https://pytorch-lightning.readthedocs.io/en/stable/common/trainer.html#default-root-dir
+
 def main(args):
+    # logger = TensorBoardLogger()
     trainer = pl.Trainer.from_argparse_args(args)
+
     model = LitNETSP(
         model = NETSP(
             d_h=args.d_hidden,
@@ -33,6 +39,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     # Program specific arguments
     parser.add_argument('--gpu_id', type=str, default='7')
+    # parser.add_argument('--version', type=int, default=999)
     # Model specific arguments
     parser = LitNETSP.add_model_arguments(parser)
     # Data Module arguments
